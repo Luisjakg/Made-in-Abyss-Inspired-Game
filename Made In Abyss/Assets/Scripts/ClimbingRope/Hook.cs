26f7fec1fa1d633abd80a5c236fde211610ce5b3
Luisjakg <52958375+Luisjakg@users.noreply.github.com>
2022-10-17 21:45:56 -0500

    Revert "Climbing rope tweaks"
    This reverts commit 81838b1ad5c5048c40a3d81b2540b7fdb654c424.

--- Made In Abyss/Assets/Scripts/ClimbingRope/Hook.cs
+++ Made In Abyss/Assets/Scripts/ClimbingRope/Hook.cs
@@ -1,14 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Obi;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Hook : MonoBehaviour
 {
-    [SerializeField] private GameObject rope;
-
     private Rigidbody rb;
     private bool targetHit;
     private Quaternion desiredRotation;
@@ -18,7 +14,6 @@ public class Hook : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        
     }
 
     private void OnCollisionEnter(Collision other)
@@ -34,15 +29,5 @@ public class Hook : MonoBehaviour
         //make sure projectile moves with target
 
         transform.SetParent(other.transform);
-    }
-
-    public bool GetTargetHit()
-    {
-        return targetHit;
-    }
-
-    public float GetMoveSpeed()
-    {
-        return rb.velocity.magnitude;
     }
 }
